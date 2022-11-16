4177a9f3e3b79922787dd1a2115a7d66d0738fe7
Kirill <sincerely.manny@gmail.com>
2022-11-16 03:42:59 +0300

    basic notifications

---
+++ components/PopUpNotifications/PopUpNotifications.tsx
@@ -0,0 +1,34 @@
+import * as Toast from '@radix-ui/react-toast';
+import { FiXCircle } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from 'store/hooks';
+import './PopUpNotifications.scss';
+import { dismissNotification } from './slice';
+
+export default function PopUpNotifications() {
+    const notifications = useAppSelector((state) => state.notifications);
+    const dispatch = useDispatch();
+    const openStateHandler = (id: string) => (isOpen: boolean) => {
+        if (!isOpen) {
+            dispatch(dismissNotification(id));
+        }
+    };
+    return (
+        <>
+            {
+                Object.entries(notifications).map(
+                    ([id, { title, message, type }]) => (
+                        <Toast.Root key={id} className={`pu-notification notification-${type || 'info'}`} onOpenChange={openStateHandler(id)}>
+                            {title && <Toast.Title className="pu-notification-title">{title}</Toast.Title> }
+                            <Toast.Description className="pu-notification-message">{message}</Toast.Description>
+                            <Toast.Close className="pu-notification-close">
+                                <FiXCircle size="25px" strokeWidth="1px" />
+                            </Toast.Close>
+                        </Toast.Root>
+                    ),
+                )
+            }
+            <Toast.Viewport className="notifications-viewport" />
+        </>
+    );
+}
