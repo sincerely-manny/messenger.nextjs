3cc9ecd5deb3c894745331941e21b336f9dccfbf
Kirill <sincerely.manny@gmail.com>
2022-11-23 05:33:04 +0300

    redeploy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,7 +10,6 @@ const prisma = new PrismaClient();
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
     session: {
-        // strategy: 'database' as SessionStrategy,
         maxAge: 30 * 24 * 60 * 60, // 30 days
         updateAge: 24 * 60 * 60, // 24 hours
     },
